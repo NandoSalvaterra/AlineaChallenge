a280f95c361867d180f8dea50dc09bc48ffaf6ba
Luiz Fernando Salvaterra <luizfernandosalvaterra@gmail.com>
2026-04-30 20:37:08 -0300

    Initial Commit

---
+++ AlineaChallenge/AlineaChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlineaChallengeApp.swift
+//  AlineaChallenge
+//
+//  Created by Luiz Fernando Salvaterra on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AlineaChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
