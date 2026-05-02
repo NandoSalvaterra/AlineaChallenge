6aca8e8448b666e834a15d0b445b70bcba172bb8
Luiz Fernando Salvaterra <luizfernandosalvaterra@gmail.com>
2026-05-02 16:35:29 -0300

    chore: add custom fonts and Font extension

---
+++ AlineaChallenge/Sources/Extensions/Font+Custom.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+extension Font {
+    static func flexaCondensedMedium(size: CGFloat) -> Font {
+        .custom("GTFlexa-CnMd", size: size)
+    }
+
+    static func instrumentSansSemiCondensedMedium(size: CGFloat) -> Font {
+        .custom("InstrumentSansSemiCondensed-Medium", size: size)
+    }
+}
